df8599ac11c3e358060fd4e0ed49a974c6fc59cc
khaledmorshed <s.m.morshed10@gmail.com>
2025-03-11 21:23:27 +0600

    local bindings

--- lib/app/bindings/initial_binding.dart
+++ lib/app/bindings/initial_binding.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:to_do/app/bindings/preference_manager_bindings.dart';
+import 'package:to_do/app/bindings/sqflite_db_bindings.dart';
 import 'package:to_do/app/bindings/tasks_bindings.dart';
 import 'package:to_do/app/data/remote/task_remote_data_source_impl.dart';
 
@@ -6,5 +8,7 @@ class InitialBinding implements Bindings {
   @override
   void dependencies() {
     TasksBindings().dependencies();
+    PreferenceManagerBindings().dependencies();
+    SqfliteDbBindings().dependencies();
   }
 }
